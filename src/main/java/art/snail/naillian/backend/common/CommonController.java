cad6ef7ee6cbd22e58745a63a247ddc370ab9429
Eunhak Lee <lee@enak.kr>
2025-02-04 17:54:27 +0900

    feat(route): 라우팅 기본 구반 구현
    관용적으로 Controller 라는 네이밍을 사용했지만 실제로는 Handler 에 가깝습니다. 모든 라우팅은
    RouteConfig 에서 결정하는데, 기존과 같이 어노테이션을 이용해서 자동 바인딩이 되도록 개선 예정입니다.

---
+++ src/main/java/art/snail/naillian/backend/common/CommonController.java
@@ -0,0 +1,17 @@
+package art.snail.naillian.backend.common;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Component
+public class CommonController {
+    public Mono<ServerResponse> streamHelloWorld(ServerRequest request) {
+        return ServerResponse.ok()
+                .contentType(MediaType.TEXT_PLAIN)
+                .body(Flux.just("Hello, ", "world!"), String.class);
+    }
+}

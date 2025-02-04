6cc75e6f2a7967b69f7d1d158e8f6480b44c21ac
Eunhak Lee <lee@enak.kr>
2025-02-04 17:47:56 +0900

    Initial commit

---
+++ src/main/java/art/snail/naillian/backend/NaillianBackendApplication.java
@@ -0,0 +1,13 @@
+package art.snail.naillian.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaillianBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaillianBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/art/snail/naillian/backend/NaillianBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package art.snail.naillian.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaillianBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

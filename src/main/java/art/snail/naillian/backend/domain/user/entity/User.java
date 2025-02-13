fb20dd943afd1a3e67ffb8679488c33d2f16343b
onehowon <flash0221@naver.com>
2025-02-13 12:50:01 +0900

    add: UserType 생성 후 CUSTOMER/STORE로 분리

--- src/main/java/art/snail/naillian/backend/domain/user/entity/User.java
+++ src/main/java/art/snail/naillian/backend/domain/user/entity/User.java
@@ -18,7 +18,7 @@ public class User {
     @Id
     private Integer id;
     private String nickname;
-    private String userType;
+    private UserType userType;
     private String profileImageUrl;
     private String registeredIp;
     private LocalDateTime createdAt;

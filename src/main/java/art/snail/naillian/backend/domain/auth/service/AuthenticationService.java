9f47ea27391a911515729d7c512f0d244a5531ce
onehowon <flash0221@naver.com>
2025-02-19 22:33:44 +0900

    chore: 토큰 reIssue시 날짜 설정 표기 잘못된 부분 수정

--- src/main/java/art/snail/naillian/backend/domain/auth/service/AuthenticationService.java
+++ src/main/java/art/snail/naillian/backend/domain/auth/service/AuthenticationService.java
@@ -1,10 +1,7 @@
 package art.snail.naillian.backend.domain.auth.service;
 
 import art.snail.naillian.backend.domain.auth.jwt.JwtProvider;
-import art.snail.naillian.backend.domain.user.entity.SocialLogin;
 import art.snail.naillian.backend.domain.user.entity.User;
-import art.snail.naillian.backend.domain.user.entity.UserType;
-import art.snail.naillian.backend.domain.user.repository.SocialLoginRepository;
 import art.snail.naillian.backend.domain.user.repository.UserRepository;
 import art.snail.naillian.backend.errors.ReportableError;
 import lombok.RequiredArgsConstructor;
@@ -12,8 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.Date;
 
 /** 회원가입 + JWT 토큰 발급 담당 */
 
@@ -22,45 +18,7 @@ import java.util.Map;
 public class AuthenticationService {
     private final JwtProvider jwtProvider;
     private final UserRepository userRepository;
-    private final SocialLoginRepository socialLoginRepository;
     private final TokenService tokenService;
-    private final KakaoAuthService kakaoAuthService;
-
-    /** 새로운 사용자 생성 */
-    // TODO: 스토어 서비스로 분리
-    private Mono<User> createNewUser(Map<String, String> userInfo) {
-        return userRepository.save(User.builder()
-                        .nickname(userInfo.get("nickname"))
-                        .userType(UserType.CUSTOMER)
-                        .registeredIp("UNKNOWN")
-                        .build())
-                .flatMap(user -> socialLoginRepository.save(
-                        SocialLogin.builder()
-                                .userId(user.getId())
-                                .platform("KAKAO")
-                                .platformUserId(userInfo.get("platformUserId"))
-                                .build()
-                ).thenReturn(user));
-    }
-
-    /** 카카오 사용자 정보 기반으로 회원가입 진행 */
-    public Mono<Map<String, String>> signUpWithKakao(String authorizationCode) {
-        return kakaoAuthService.getAccessToken(authorizationCode)
-                .flatMap(kakaoAuthService::getUserInfo)
-                .flatMap(userInfo -> kakaoAuthService.findUserByKakaoId(userInfo.get("platformUserId"))
-                        .switchIfEmpty(createNewUser(userInfo))
-                )
-                .flatMap(user -> {
-                    String accessToken = jwtProvider.generateAccessToken(user.getId());
-                    String refreshToken = jwtProvider.generateRefreshToken(user.getId());
-
-                    return Mono.just(new LinkedHashMap<>(Map.of(
-                            "nickname", user.getNickname(),
-                            "accessToken", accessToken,
-                            "refreshToken", refreshToken
-                    )));
-                });
-    }
 
 
     /** Access Token 검증 및 사용자 ID 추출 */
@@ -93,7 +51,7 @@ public class AuthenticationService {
                     if (userId == null) {
                         return Mono.error(new ReportableError(HttpStatus.BAD_REQUEST, "유효하지 않은 토큰입니다."));
                     }
-                    return Mono.just(jwtProvider.generateAccessToken(userId));
+                    return Mono.just(jwtProvider.generateAccessToken(userId, new Date()));
                 });
     }
 

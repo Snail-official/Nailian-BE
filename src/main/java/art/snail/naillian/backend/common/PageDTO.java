de8b6a84be1c3421c21d9549013aa3a504fb362c
Eunhak Lee <lee@enak.kr>
2025-02-23 18:45:53 +0900

    feat: pagination 직렬화 구현

---
+++ src/main/java/art/snail/naillian/backend/common/PageDTO.java
@@ -0,0 +1,46 @@
+package art.snail.naillian.backend.common;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.io.IOException;
+import java.util.List;
+
+@JsonSerialize(using = PageDTO.Serializer.class)
+public class PageDTO<T> extends PageImpl<T> {
+    public PageDTO(List<T> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    @JsonComponent
+    @SuppressWarnings("unchecked")
+    static class Serializer extends JsonSerializer<PageDTO<?>> {
+        @Override
+        public void serialize(PageDTO page, JsonGenerator gen, SerializerProvider serializerProvider) throws IOException {
+            gen.writeStartObject();
+
+            gen.writeNumberField("page", page.getNumber());
+            gen.writeNumberField("size", page.getSize());
+            gen.writeNumberField("totalElements", page.getTotalElements());
+            gen.writeNumberField("totalPages", page.getTotalPages());
+
+            gen.writeFieldName("content");
+            gen.writeStartArray();
+            page.getContent().forEach(content -> {
+                try {
+                    gen.writeObject(content);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            gen.writeEndArray();
+
+            gen.writeEndObject();
+        }
+    }
+}

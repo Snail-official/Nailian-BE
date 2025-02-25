77ebcdba9fae8498266ec4c1eb2832622be3faef
Eunhak Lee <lee@enak.kr>
2025-02-26 04:39:15 +0900

    chore(nail-set): nailset 이 없는 경우 대응

--- src/main/java/art/snail/naillian/backend/domain/nail/controller/NailSetController.java
+++ src/main/java/art/snail/naillian/backend/domain/nail/controller/NailSetController.java
@@ -4,7 +4,9 @@ import art.snail.naillian.backend.common.CommonResponse;
 import art.snail.naillian.backend.domain.nail.dto.NailImageUrlDTO;
 import art.snail.naillian.backend.domain.nail.dto.NailSetEmbedDTO;
 import art.snail.naillian.backend.domain.nail.service.NailService;
+import art.snail.naillian.backend.errors.ReportableError;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +24,7 @@ public class NailSetController {
             @PathVariable("id") Long id
     ) {
         return nailService.getNailsBySetId(id)
+                .switchIfEmpty(Mono.error(new ReportableError(HttpStatus.NOT_FOUND, "네일 세트를 찾을 수 없습니다.")))
                 .map(NailImageUrlDTO::from)
                 .collectList()
                 .map(list -> new NailSetEmbedDTO<>(id, list))

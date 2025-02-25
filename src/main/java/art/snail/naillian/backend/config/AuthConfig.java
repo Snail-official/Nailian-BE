f50ada59010bf6dab608e3963f58eb07f4dda6e0
Eunhak Lee <lee@enak.kr>
2025-02-26 06:48:53 +0900

    feat: endpoint 에 따라 인증 적용하도록

--- src/main/java/art/snail/naillian/backend/config/AuthConfig.java
+++ src/main/java/art/snail/naillian/backend/config/AuthConfig.java
@@ -1,10 +1,15 @@
 package art.snail.naillian.backend.config;
 
+import art.snail.naillian.backend.domain.auth.jwt.JwtAuthFilter;
+import art.snail.naillian.backend.domain.auth.jwt.JwtProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -14,10 +19,21 @@ public class AuthConfig {
     };
 
     @Bean
-    SecurityWebFilterChain defaultSecurityFilterChain(ServerHttpSecurity http) throws Exception {
+    SecurityWebFilterChain defaultSecurityFilterChain(
+            ServerHttpSecurity http,
+            JwtProvider jwtProvider
+    ) throws Exception {
         return http
-                .authorizeExchange(exchange -> exchange.anyExchange().permitAll())
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)  //
+                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())  // stateless authentication
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers(HttpMethod.OPTIONS).permitAll()
+                        .pathMatchers("/auth/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .addFilterAt(new JwtAuthFilter(jwtProvider), SecurityWebFiltersOrder.HTTP_BASIC)
                 .build();
     }
 }

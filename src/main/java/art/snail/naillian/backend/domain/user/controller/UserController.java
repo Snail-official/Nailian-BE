124191f2a5807d5d903d20a9dfc415346ebddce7
onehowon <flash0221@naver.com>
2025-02-26 15:12:15 +0900

    refactor: 공통응답부 및 DTO로 응답 교체 및 인증 로직 리팩터링

--- src/main/java/art/snail/naillian/backend/domain/user/controller/UserController.java
+++ src/main/java/art/snail/naillian/backend/domain/user/controller/UserController.java
@@ -2,14 +2,9 @@ package art.snail.naillian.backend.domain.user.controller;
 
 
 import art.snail.naillian.backend.common.CommonResponse;
-import art.snail.naillian.backend.domain.auth.jwt.UserAuthByTokenPayload;
-import art.snail.naillian.backend.domain.auth.utils.AuthUtil;
 import art.snail.naillian.backend.domain.user.dto.UserResponseDTO;
 import art.snail.naillian.backend.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.ReactiveSecurityContextHolder;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
@@ -20,26 +15,16 @@ import java.util.Map;
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
-    private final Mono<SecurityContext> ctx = ReactiveSecurityContextHolder.getContext();
 
     /** 사용자 조회 */
     @GetMapping("/me")
     public Mono<CommonResponse<UserResponseDTO>> getUserInfo() {
-        return AuthUtil.getContextUser(ctx)
-                .map(UserAuthByTokenPayload::getUserId)
-                .flatMap(userService::getUserById)
-                .map(UserResponseDTO::from)
-                .map(dto -> CommonResponse.success(dto, "사용자 정보 조회 성공"));
+        return userService.getUserInfo();
     }
 
     /** 닉네임 변경 (온보딩 여부 반영) */
     @PatchMapping("/me/nickname")
-    public Mono<ResponseEntity<Map<String ,Object>>> updateNickname(
-            @RequestBody Map<String, String> requestBody
-    ) {
-        return AuthUtil.getContextUser(ctx)
-                .map(UserAuthByTokenPayload::getUserId)
-                .flatMap(userId -> userService.updateNickname(userId, requestBody.get("nickname")))
-                .map(ResponseEntity::ok);
+    public Mono<CommonResponse<UserResponseDTO>> updateNickname(@RequestBody Map<String, String> requestBody){
+        return userService.updateNickname(requestBody.get("nickname"));
     }
 }

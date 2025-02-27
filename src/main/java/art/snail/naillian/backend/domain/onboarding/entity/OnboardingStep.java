d9ed8a1dadf32eaf5892765d5dad8a0cd02d5d51
Eunhak Lee <lee@enak.kr>
2025-02-27 16:33:37 +0900

    feat(enum): OnboardingStep 의 직렬화 이름을 Swagger 에 맞게 변경

--- src/main/java/art/snail/naillian/backend/domain/onboarding/entity/OnboardingStep.java
+++ src/main/java/art/snail/naillian/backend/domain/onboarding/entity/OnboardingStep.java
@@ -1,8 +1,16 @@
 package art.snail.naillian.backend.domain.onboarding.entity;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Getter;
 
+import java.io.IOException;
+
 @Getter
+@JsonSerialize(using = OnboardingStep.OnboardingStepSerializer.class)
 public enum OnboardingStep {
     NICKNAME(0, 1),
     PREFERENCES(1, 2),
@@ -14,5 +22,17 @@ public enum OnboardingStep {
     OnboardingStep(int order, int requiredVersion) {
         this.bitmask = 1 << order;
         this.requiredVersion = requiredVersion;
+    }
+
+    public String getSerializeName() {
+        return "Onboarding" +
+                PropertyNamingStrategies.UpperCamelCaseStrategy.INSTANCE.translate(name().toLowerCase());
+    }
+
+    public static class OnboardingStepSerializer extends JsonSerializer<OnboardingStep> {
+        @Override
+        public void serialize(OnboardingStep step, JsonGenerator generator, SerializerProvider provider) throws IOException {
+            generator.writeObject(step.getSerializeName());
+        }
     }
 }

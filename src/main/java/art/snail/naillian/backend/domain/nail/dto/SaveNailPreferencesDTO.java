290f173d6358ff79f6c3893394f136de4e43849d
onehowon <flash0221@naver.com>
2025-03-02 16:20:31 +0900

    chore: UserPreferencesRepository를 Bean으로 인식하지 않는 오류 해결을 위해 NoArgs 어노테이션 추가

--- src/main/java/art/snail/naillian/backend/domain/nail/dto/SaveNailPreferencesDTO.java
+++ src/main/java/art/snail/naillian/backend/domain/nail/dto/SaveNailPreferencesDTO.java
@@ -3,12 +3,14 @@ package art.snail.naillian.backend.domain.nail.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class SaveNailPreferencesDTO {
     private List<Integer> preferences;
 

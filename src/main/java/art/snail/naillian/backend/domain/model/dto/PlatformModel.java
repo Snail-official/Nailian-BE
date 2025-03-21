4c67742d18bddb3e39b15234f645d055ec531fec
onehowon <flash0221@naver.com>
2025-03-21 14:48:17 +0900

    refactor: DTO를 바뀐 json 형식에 맞춰 변경

---
+++ src/main/java/art/snail/naillian/backend/domain/model/dto/PlatformModel.java
@@ -0,0 +1,14 @@
+package art.snail.naillian.backend.domain.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlatformModel {
+
+    private ModelDetail segmentation;
+    private ModelDetail detection;
+}

bd18f6f57caa96682f3285c838e32dd2d03fa7fa
Eunhak Lee <lee@enak.kr>
2025-04-03 15:19:49 +0900

    feat: CursorPageDTO 도입

---
+++ src/main/java/art/snail/naillian/backend/common/CursorPageDTO.java
@@ -0,0 +1,54 @@
+package art.snail.naillian.backend.common;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.Getter;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.io.IOException;
+import java.util.List;
+
+@JsonSerialize(using = CursorPageDTO.Serializer.class)
+public class CursorPageDTO<T> extends PageImpl<T> {
+    @Getter
+    private final String cursor;
+
+    public CursorPageDTO(String cursor, List<T> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+        this.cursor = cursor;
+    }
+
+    @JsonComponent
+    static class Serializer extends JsonSerializer<CursorPageDTO<?>> {
+
+        @Override
+        public void serialize(CursorPageDTO<?> page, JsonGenerator gen, SerializerProvider provider) throws IOException {
+            gen.writeStartObject();
+
+            // pageInfo
+            {
+                gen.writeFieldName("pageInfo");
+                gen.writeStartObject();
+                gen.writeNumberField("totalElements", page.getTotalElements());
+                gen.writeStringField("cursor", page.getCursor());
+                gen.writeEndObject();
+            }
+
+            // content[]
+            {
+                gen.writeFieldName("content");
+                gen.writeStartArray();
+                for (Object content : page.getContent()) {
+                    gen.writeObject(content);
+                }
+                gen.writeEndArray();
+            }
+
+            gen.writeEndObject();
+        }
+    }
+}

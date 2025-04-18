e2eed950395a26c4ef6396dc9dcab075c5d64573
Eunhak Lee <lee@enak.kr>
2025-04-18 16:30:21 +0900

    refactor: 읽기 전용으로 쓰는 payload 는 @Getter 만 선언

--- src/main/java/art/snail/naillian/backend/domain/user/dto/EventSubmissionDTO.java
+++ src/main/java/art/snail/naillian/backend/domain/user/dto/EventSubmissionDTO.java
@@ -1,9 +1,9 @@
 package art.snail.naillian.backend.domain.user.dto;
 
-import lombok.Data;
+import lombok.Getter;
 
 
-@Data
+@Getter
 public class EventSubmissionDTO {
     private String userInfo;
     private Integer nailSetId;

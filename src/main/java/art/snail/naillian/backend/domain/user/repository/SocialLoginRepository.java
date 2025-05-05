778d90ac591470fbaa1f24f2ea9977a8d8648959
onehowon <flash0221@naver.com>
2025-05-05 17:13:01 +0900

    fix: 회원탈퇴시 재가입 후 온보딩 화면이 돌아오지 않는 부분 픽스

--- src/main/java/art/snail/naillian/backend/domain/user/repository/SocialLoginRepository.java
+++ src/main/java/art/snail/naillian/backend/domain/user/repository/SocialLoginRepository.java
@@ -1,10 +1,17 @@
 package art.snail.naillian.backend.domain.user.repository;
 
 import art.snail.naillian.backend.domain.user.entity.SocialLogin;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
 
 
 public interface SocialLoginRepository extends ReactiveCrudRepository<SocialLogin, Integer> {
+    @Query("""
+    SELECT *
+    FROM social_login
+    WHERE platform_user_id = :platformUserId
+      AND deleted_at IS NULL
+""")
     Mono<SocialLogin> findByPlatformUserId(String platformUserId);
 }
--- src/main/java/art/snail/naillian/backend/domain/user/repository/UserRepository.java
+++ src/main/java/art/snail/naillian/backend/domain/user/repository/UserRepository.java
@@ -1,9 +1,17 @@
 package art.snail.naillian.backend.domain.user.repository;
 
 import art.snail.naillian.backend.domain.user.entity.User;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
 
 public interface UserRepository extends ReactiveCrudRepository<User, Integer> {
+
+    @Query("""
+    SELECT *
+    FROM user
+    WHERE nickname = :nickname
+      AND deleted_at IS NULL
+""")
     Mono<User> findByNickname(String nickname);
 }
